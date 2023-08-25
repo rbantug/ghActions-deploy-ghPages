715d0d76fb93a19935d5e2661e38f590d2b23b55
rbantug <ronbantug22@gmail.com>
2023-08-25 17:37:20 +0800

    added conditional element

--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -1,8 +1,14 @@
 <template>
     <div>
         <h1>Hey</h1>
-        <div data-testid="count">Count: {{ count }}</div>
-        <button data-testid="btnIncrement" @click="increment">Click me!</button>
+        <div>
+            <div data-testid="count">Count: {{ count }}</div>
+            <button data-testid="btnIncrement" @click="increment">Click me!</button>
+        </div>
+        <div>
+            <div v-if="state" data-testid="conditional">I'm part of the DOM</div>
+            <button data-testid="conditionalBtn" @click="updateState">Show element</button>
+        </div>
     </div>
 </template>
 
@@ -12,5 +18,10 @@ import { ref } from 'vue'
 const count = ref(0)
 function increment() {
     count.value += 1
+}
+
+const state = ref(false)
+function updateState() {
+    state.value = false
 }
 </script>
